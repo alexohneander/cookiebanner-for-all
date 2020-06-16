81722c034c4c50b22446f0996128d96a0214ff80
Alex Wellnitz <moin@wellnitz-alex.de>
2020-06-16 17:17:56 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="cookiebanner">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Banner/>
+  </div>
+</template>
+
+<script>
+import Banner from './components/Banner.vue'
+
+export default {
+  name: 'cookiebanner',
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style>
+#cookiebanner {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="cookiebanner-box open">
+        <div class="cookiebanner-box__inner">
+            <img src="https://www.pulse-advertising.com/wp-content/themes/pulse/assets/images/cookies_image.png">
+            <div class="title uppercase">Have a Cookie</div>
+
+            <div class="desc">We use cookies. Just to track the performance of the website, and to provide a better user experience. We store no personal details. <br><br> Read our <a href="https://www.pulse-advertising.com/privacy/">Cookie Statement</a> for more details. <a class="accept_cookies">Accept cookies.</a>        </div>
+
+            <div class="button">
+                <button class="accept_cookies">Accept Cookies</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .cookiebanner-box{
+        position: fixed;
+        bottom: 100px;
+        background: black;
+        width: 255px;
+        z-index: 20000;
+        color: white;
+    }
+
+    .cookiebanner-box.open{
+        transition: all 0.6s ease-out 0.3s;
+        right: 50px;
+    }
+
+    .cookiebanner-box .cookiebanner-box__inner{
+        position: relative;
+        padding: 40px 40px 30px 40px;
+        text-align: center;
+    }
+</style>
